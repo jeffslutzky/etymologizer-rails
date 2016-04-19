0ef4369a3d6196e4a13f378e20489c62430999e7
jeffslutzky <jeffslutzky@gmail.com>
2016-04-19 18:50:16 -0400

    fix directory structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+
+  get '/api/words', to: 'words#index'
+
+  resources :words, except: [:new, :edit]
+  namespace :api, defaults: {format: :json} do
+    scope module: :v1 do
+
+    end
+  end
+end
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +69,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
